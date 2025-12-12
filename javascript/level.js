38b44e15ef45444cad3d84f8a0f7027b64a5a808
anmvillarica <b2029anmvillarica@pshs.edu.ph>
2025-12-12 10:31:36 +0800

    Fix hyperlinks and level 1

--- javascript/level.js
+++ javascript/level.js
@@ -5,6 +5,7 @@ var levelSelector = 0;
 
 var sbt = document.getElementById("submit_btn");
 var sprite = document.getElementById("sprite");
+var rbt = document.getElementById("reset_btn");
 
 var bar1 = document.getElementById("bar1");
 var bar2 = document.getElementById("bar2");
@@ -12,48 +13,51 @@ var bar3 = document.getElementById("bar3");
 
 var progress_bar = document.getElementById("progress_bar");
 
+var var_name = ""; 
+var id = "";
+var inp = "";
+
+var incorrect = [];
+
 function checkInput(level){
-    progress_bar.style.backgroundColor = "#262626";
-
-    var incorrect = [];
-
-    for (let i = 0; i < incorrect.length; i++)
-        incorrect.pop; 
-
+    alert("this works");
 
     for (let i = 0; i < 3; i++)
     {
-        var var_name = "inp" + (i + 1);
-        var id = "blank" + (i + 1);
-        var var_name = document.getElementById(id).value;
+        var_name = "inp" + (i + 1);
+        id = "blank" + (i + 1);
+        inp = document.getElementById(id).value;
 
         switch(level)
         {
             case 1: 
-                if (var_name != LEVEL1_STR[i])
+                if (inp != LEVEL1_STR[i])
                     incorrect.push(i);
                 break; 
             case 2: 
-                if (var_name != LEVEL2_STR[i])
+                if (inp != LEVEL2_STR[i])
                     incorrect.push(i);
                 break; 
             case 3: 
-                if (var_name != LEVEL3_STR[i])
+                if (inp != LEVEL3_STR[i])
                     incorrect.push(i);
                 break; 
         }
         
     }
 
-    if (incorrect.length == 2)
+    if (incorrect.length == 1)
+        bar2.style.backgroundColor = "white"
+    if (incorrect.length <= 2)
     {
         bar1.style.backgroundColor = "white";
-        if (incorrect.length == 1)
-            bar2.style.backgroundColor = "white"
-        
     }
     if (incorrect.length == 0)
     {
+        bar1.style.backgroundColor = "white";
+        bar2.style.backgroundColor = "white";
+        bar3.style.backgroundColor = "white";
+
         sprite.style.animation = `1s ease level1animation`;
         setTimeout(() => {
             sprite.innerHTML = "<img src='../media/sprites/pengy2.png' alt='Pengy'>";
@@ -62,3 +66,20 @@ function checkInput(level){
     }
 }
 
+function refresh() {    
+    alert("Gumagana to");
+    sprite.innerHTML = "<img src='../media/sprites/pengy1.png' alt='Pengy'>";
+    
+    sprite.style.transform = "translateX(0px)";
+
+    bar1.style.backgroundColor = "#262626";
+    bar2.style.backgroundColor = "#262626";
+    bar3.style.backgroundColor = "#262626";
+
+    for (let i = 1; i <= incorrect.length; i++)
+        incorrect.pop(); 
+    
+    var_name = ""; 
+    id = "";
+    inp = "";
+}
